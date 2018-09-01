4b8f952d13aa1986a2f8173cd51c3817764dcc0b
yaffa7 <bmcmahon2@mail.csuchico.edu>
2018-09-01 14:36:06 -0700

    added resume content

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Document, Page } from 'react-pdf/dist/entry.webpack';
+import './resume.css';
+import resume from '../pdf/Resume-2018.pdf';
 
-const Resume = () => {
-    return (
-        <section className='content'>   
-            <h1>Resume</h1>
-        </section>
-    )
+class Resume extends Component {
+    state = {
+        numPages: null,
+        pageNumber: 1
+    }
+
+    onDocumentLoad = ({ numPages }) => {
+        this.setState({ numPages });
+    }
+
+    render() {
+        const { pageNumber, numPages } = this.state;
+        return (
+            <section className='content'>
+                <span className="breadcrumbs">
+                    <Link to="/">Home</Link> > <b>Resume</b>
+                </span>
+                <div className="pdf-content">
+                    <Document
+                        file={resume}
+                        onLoadSuccess={this.onDocumentLoad}
+                        loading=""
+                    >
+                        <Page className="pdf"
+                            pageNumber={pageNumber}
+                            scale={2}
+                        />
+                    </Document>
+                </div>
+            </section>
+        )
+    }
 }
 
 export default Resume
