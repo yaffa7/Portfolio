efc454cbd2b8cb8e39e9fdbfe116551422e52854
yaffa7 <bmcmahon2@mail.csuchico.edu>
2019-01-08 20:39:55 -0800

    desktop and mobile styles

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -18,8 +18,8 @@ const Projects = () => {
             </span>
             <div className="container-center">
                 <ul className="project-list">
-                    <ProjectItem projectName="Todos Plus" technologies="angular,express,mongo,node"imgURL={todosplus} desc="Todos Plus is a Full-Stack javascript app meant for creating a log of tasks and keep track of things you gotta do. The frontend is an Angular 6 app supported by a Node app for the backend using Mongodb for the database."/>
-                    <ProjectItem projectName="Quick Poll (Front End)" technologies="react" imgURL="https://via.placeholder.com/200x200" />
+                    <ProjectItem projectName="Todos Plus" demo="https://sleepy-fortress-89639.herokuapp.com" technologies="angular,express,mongo,node" imgURL={todosplus} desc="Todos Plus is a Full-Stack javascript app meant for creating a log of tasks and keep track of things you gotta do. The frontend is an Angular 6 app supported by a Node app for the backend using Mongodb for the database." />
+                    <ProjectItem projectName="Quick Poll (Front End)" demo="http://yaffa7.github.io/quick-poll" technologies="react" imgURL="https://via.placeholder.com/200x200" />
                     <ProjectItem projectName="AGDQ Quick Viewer" technologies="mongo,node,react" imgURL="https://via.placeholder.com/200x200" />
                 </ul>
             </div>
@@ -31,65 +31,69 @@ const ProjectItem = (props) => {
     return (
         <li className="project-item">
             <section className="project-header">
-                <span className="project-title">{props.projectName}</span>
-                <a href="https://sleepy-fortress-89639.herokuapp.com">
-                    <button className="project-link">Demo <img src={externalicon} alt="external link icon"/></button>
-                </a>
+                <div className="l-header">
+                    <span className="project-title">{props.projectName}</span>
+                    <a href={props.demo}>
+                        <button className="project-link">Demo <img src={externalicon} alt="external link icon" /></button>
+                    </a>
+                </div>
+                <div className="r-header">
+                    <div className="project-technologies">
+                        {props.technologies.split(',').map(t => {
+                            if (t === 'angular') {
+                                return <AngularIcon key={t} />
+                            }
+                            if (t === 'express') {
+                                return <ExpressIcon key={t} />
+                            }
+                            if (t === 'node') {
+                                return <NodeIcon key={t} />
+                            }
+                            if (t === 'mongo') {
+                                return <MongoIcon key={t} />
+                            }
+                            if (t === 'react') {
+                                return <ReactIcon key={t} />
+                            }
+                        })}
+                    </div>
+                </div>
             </section>
             <section className="project-content">
                 <img src={props.imgURL} alt="" className="project-img" />
                 <img src={props.imgURL} alt="" className="project-img" />
             </section>
-                <p className="project-description">{props.desc ? props.desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, quod! Velit in ducimus temporibus asperiores perspiciatis ea sed delectus impedit?"}</p>
-            <div className="project-technologies">
-                { props.technologies.split(',').map(t => {
-                    if (t === 'angular') {
-                        return <AngularIcon key={t}/>
-                    }
-                    if (t === 'express') {
-                        return <ExpressIcon key={t}/>
-                    }
-                    if (t === 'node') {
-                        return <NodeIcon key={t}/>
-                    }
-                    if (t === 'mongo') {
-                        return <MongoIcon key={t}/>
-                    }
-                    if (t === 'react'){
-                        return <ReactIcon key={t}/>
-                    }
-                })}
-            </div>
+            <p className="project-description">{props.desc ? props.desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, quod! Velit in ducimus temporibus asperiores perspiciatis ea sed delectus impedit?"}</p>
         </li>
     )
 }
 
 const AngularIcon = () => {
     return (
-        <div className="technology"><img src={angularicon} alt="angular icon"/></div>
+        <div className="technology"><img src={angularicon} alt="angular icon" /></div>
     )
 }
 
 const ExpressIcon = () => {
     return (
-        <div className="technology"><img src={expressicon} alt="express icon"/></div>
+        <div className="technology"><img src={expressicon} alt="express icon" /></div>
     )
 }
 
 const MongoIcon = () => {
     return (
-        <div className="technology"><img src={mongoicon} alt="mongodb icon"/></div>
+        <div className="technology"><img src={mongoicon} alt="mongodb icon" /></div>
     )
 }
 
 const NodeIcon = () => {
     return (
-        <div className="technology"><img className="node" src={nodeicon} alt="node icon"/></div>
+        <div className="technology"><img className="node" src={nodeicon} alt="node icon" /></div>
     )
 }
 const ReactIcon = () => {
     return (
-        <div className="technology"><img src={reacticon} alt="react icon"/></div>
+        <div className="technology"><img src={reacticon} alt="react icon" /></div>
     )
 }
 
