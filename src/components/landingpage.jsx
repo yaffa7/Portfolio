158a294cf89d2588278b19869cd01d8776c5c155
yaffa7 <brendan6z@gmail.com>
2019-02-15 17:46:39 -0800

    added github/linkedin/meetup icons to landing page

--- src/components/landingpage.jsx
+++ src/components/landingpage.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './landingpage.css'
-
+import linkedinIcon from '../icons/linkedin.svg'
+import githubicon from '../icons/github.svg'
+import meetupIcon from '../icons/meetup.svg'
 
 class LandingPage extends Component {
     render() {
@@ -8,7 +10,18 @@ class LandingPage extends Component {
             <div>
                 <div className="title-container">
                     <h1 className="title">Brendan McMahon</h1>
-                    <h4 className="subtitle">Front End Developer</h4>
+                    <div className="subtitle">
+                        <h4>Front End Developer</h4>
+                        <a href="https://github.com/yaffa7/" target="_blank">
+                            <img src={githubicon} alt="" />
+                        </a>
+                        <a href="https://www.linkedin.com/in/brendan-mcmahon-009485101/" target="_blank">
+                            <img src={linkedinIcon} alt="" />
+                        </a>
+                        <a href="https://www.meetup.com/Code-and-Coffee-SD/">
+                            <img src={meetupIcon} alt=""/>
+                        </a>
+                    </div>
                 </div>
             </div>
         )
