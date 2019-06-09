2553e747c0f78e5bf017a28197ddcf8940c84671
Brendan <brendan6z@gmail.com>
2019-06-09 16:00:55 -0700

    mobile nav mostly working

--- src/components/Icons/Hamburger/Hamburger.jsx
+++ src/components/Icons/Hamburger/Hamburger.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Hamburger.css'
 
-const Hamburger = () => {
+const Hamburger = (props) => {
     return (
-        <div className="hamburger container">
+        <div className={props.expanded ? 'hamburger container-expanded' : 'hamburger container'}>
             <div className="box"></div>
             <div className="box"></div>
             <div className="box"></div>
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -33,28 +33,64 @@ class Nav extends Component {
         }, function () { console.log(this.state); })
     }
 
+    handleClick = () => {
+        this.setState((state) => {
+            return {
+                y: state.y,
+                expanded: false
+            }
+        })
+    }
+
     render() {
         const shouldBeOpaque = this.state && this.state.y > 65;
-        return (
-            <div>
-                <nav className={shouldBeOpaque ? 'navbar-opaque' : 'navbar'}>
-                    <ul className="item-list">
-                        <li className="nav-item">
-                            <Link to="/projects">Projects</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/resume">Resume</Link>
-                        </li>
-                        <li className="nav-item">
-                            <a href="https://github.com/yaffa7" className="nav-link">Github</a>
-                        </li>
-                        <li className="nav-item">
-                            <HamburgerIcon/>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        )
+        let expanded = this.state && this.state.expanded;
+
+
+        { if (expanded == false) {
+            return (
+                <div>
+                    <nav className={shouldBeOpaque ? 'navbar-opaque' : 'navbar'}>
+                        <ul className="item-list">
+                            <li className="nav-item">
+                                <Link to="/projects">Projects</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/resume">Resume</Link>
+                            </li>
+                            <li className="nav-item">
+                                <a href="https://github.com/yaffa7" className="nav-link">Github</a>
+                            </li>
+                            <li className="nav-item" onClick={this.updateExpanded}>
+                                <HamburgerIcon expanded={false}/>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            )}
+            else {
+                return (
+                    <div>
+                    <nav className={shouldBeOpaque ? 'navbar-opaque' : 'navbar'}>
+                        <div onClick={this.updateExpanded}>
+                            <HamburgerIcon expanded={true}/>
+                        </div>
+                        <ul className="item-list-expanded" onClick={this.handleClick}>
+                            <li className="nav-item-expanded">
+                                <Link to="/projects">Projects</Link>
+                            </li>
+                            <li className="nav-item-expanded" onClick={this.handleClick}>
+                                <Link to="/resume">Resume</Link>
+                            </li>
+                            <li className="nav-item-expanded" onClick={this.handleClick}>
+                                <a href="https://github.com/yaffa7" className="nav-link">Github</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                )
+            }
+        }
     }
 }
 
