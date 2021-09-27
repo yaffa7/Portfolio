b50aa74b1abe1903c5d3d442247788b1de5d9a56
yaffa7 <bmcmahon2@mail.csuchico.edu>
2021-09-27 15:42:31 -0700

    fixed 404 on refreash, remove class component

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -4,28 +4,26 @@ import linkedinIcon from '../../icons/linkedin.svg'
 import githubicon from '../../icons/github.svg'
 import meetupIcon from '../../icons/meetup.svg'
 
-class LandingPage extends Component {
-    render() {
-        return (
-            <div>
-                <div className="title-container">
-                    <h1 className="title">Brendan McMahon</h1>
-                    <div className="subtitle">
-                        <h4>Developer</h4>
-                        <a href="https://github.com/yaffa7/" target="__blank">
-                            <img src={githubicon} alt="" />
-                        </a>
-                        <a href="https://www.linkedin.com/in/brendan-mcmahon-009485101/" target="__blank">
-                            <img src={linkedinIcon} alt="" />
-                        </a>
-                        <a href="https://www.meetup.com/Code-and-Coffee-SD/" target="__blank">
-                            <img src={meetupIcon} alt=""/>
-                        </a>
-                    </div>
+const LandingPage = () => {
+    return (
+        <div>
+            <div className="title-container">
+                <h1 className="title">Brendan McMahon</h1>
+                <div className="subtitle">
+                    <h4>I Build Things</h4>
+                    <a href="https://github.com/yaffa7/" target="__blank">
+                        <img src={githubicon} alt="" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/brendan-mcmahon-009485101/" target="__blank">
+                        <img src={linkedinIcon} alt="" />
+                    </a>
+                    <a href="https://www.meetup.com/Code-and-Coffee-SD/" target="__blank">
+                        <img src={meetupIcon} alt="" />
+                    </a>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default LandingPage
